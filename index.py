d4433b158afe437a34bd8d655a70e612be7e5784
Konda Shivaradhan <38292566+KondaShivaradhan@users.noreply.github.com>
2023-06-16 22:56:27 -0500

    added requirements

--- index.py
+++ index.py
@@ -1,32 +1,4 @@
-# from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.chrome.options import Options
-# import os
-# import subprocess
-# import getpass
-
-# # Get the currently logged-in user account name
-# username = getpass.getuser()
-# command = 'chrome.exe --remote-debugging-port=8989'
-# working_directory = 'C:\\Program Files\\Google\\Chrome\\Application'
-# subprocess.run(command, shell=True, cwd=working_directory)
-# file_path = 'driver'
-# print(file_path)
-# # Check if the path is a file
-# if os.path.isfile(file_path):
-#     print(f"The path '{file_path}' points to a file.")
-# else:
-#     print(f"The path '{file_path}' does not point to a file or the file does not exist.")
-#     driver = webdriver.Chrome(ChromeDriverManager(path=file_path).install())
-# # Open a URL in the Chrome browser
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_experimental_option("debuggerAddress", "localhost:8989")
-# driver = webdriver.Chrome(options=chrome_options)
-# url = 'https://www.youtube.com/@aajtak/live'
-# driver.get(url)
-
-# # C:\Program Files\Google\Chrome\Application
-# # driver.quit()
+# pip install -r requirements.txt
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
